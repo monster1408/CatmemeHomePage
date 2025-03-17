e887450bd013554577a92fa440fd73c22f555f30
monster1408 <yukiya.toyohara@hotmail.com>
2025-03-17 22:00:46 +0900

    Update cat.js

--- cat.js
+++ cat.js
@@ -2,7 +2,7 @@ var formPass = document.getElementById("pass");
 var catimg = document.getElementById("cat");
 var images = ["やぎ","ハッピー","何やってんだ","死にそう","痛い","米津","驚き"]
 function imgselect(num){
-  catimg.src = images[num];
+  catimg.src = images[num] + ".GIF";
 }
 function passfanction(){
   if(formPass.value == "Za7630"){
