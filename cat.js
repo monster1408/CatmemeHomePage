7705fabd0076ac5772ab37b2afeefcd29ea2e14c
monster1408 <yukiya.toyohara@hotmail.com>
2025-03-17 21:50:29 +0900

    Update cat.js

--- cat.js
+++ cat.js
@@ -1,6 +1,6 @@
 var formPass = document.getElementById("pass");
 var catimg = document.getElementById("cat");
-var images = ["やぎ,"ハッピー","何やってんだ","死にそう","痛い","米津","驚き"]
+var images = ["やぎ","ハッピー","何やってんだ","死にそう","痛い","米津","驚き"]
 function imgselect(num){
   catimg.src = images[num];
 }
