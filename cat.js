e1835f0fc7c343acaae1cce6544a87be158ffc67
monster1408 <yukiya.toyohara@hotmail.com>
2025-03-17 21:47:12 +0900

    Update cat.js

--- cat.js
+++ cat.js
@@ -1,13 +1,13 @@
 var formPass = document.getElementById("pass");
-var formPass = document.getElementById("pass");
+var catimg = document.getElementById("cat");
 var images = ["やぎ,"ハッピー","何やってんだ","死にそう","痛い","米津","驚き"]
-function imgselect(){
-  
+function imgselect(num){
+  catimg.src = images[num];
 }
 function passfanction(){
   if(formPass.value == "Za7630"){
     formPass.style.display = "none";
     document.getElementById("passbutton").style.display = "none";
-    imgselect()
+    imgselect(0)
   }
 }
